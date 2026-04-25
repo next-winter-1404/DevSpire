e573389c47fdb2597621a8b13d8288e6d49af654
Abolfazl <abolfazlmnf83@gmail.com>
2026-04-26 01:12:44 +0330

    edit structure

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,3 +1,6 @@
+import Footer from "@/components/Footer/Footer";
+import Header from "@/components/Header/Header";
+
 export default function MainLayout({
   children,
 }: {
@@ -5,9 +8,11 @@ export default function MainLayout({
 }) {
   return (
     <>
+      <Header />
       <main className="px-4 sm:px-6 lg:px-8 py-8 sm:py-12 lg:py-16 container">
         {children}
       </main>
+      <Footer />
     </>
   );
 }
--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -2,10 +2,5 @@ import Header from "@/components/Header/Header";
 import Footer from "@/components/Footer/Footer";
 
 export default function Home() {
-  return (
-    <div>
-      <Header/>
-      <Footer/>
-    </div>
-  );
+  return <div>home page</div>;
 }
