d42d1a10405992834b14b9a6f5105ce23d273d3c
Parsa <mparsabahredar@gmail.com>
2026-04-26 13:35:21 +0330

    commit

---
+++ src/icons/ArrowRight.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ArrowRight = () => {
+  return (
+    <svg width="16" height="30" viewBox="0 0 16 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M1 29L15 15L1 1" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+    </svg>
+  )
+}
+
+export default ArrowRight
