03cb8383f63d060296b5c72381f801cf1821d0df
Parsa <mparsabahredar@gmail.com>
2026-05-10 18:13:33 +0330

    commit

--- src/modules/mortgageRent/components/MortgageRentList.tsx
+++ src/modules/mortgageRent/components/MortgageRentList.tsx
@@ -1,5 +1,4 @@
-import FastReserveCard from '@/modules/fastReserve/components/FastReserveCard'
-import { MOCK_DATA } from '@/modules/fastReserve/mocks/data'
+import HouseCard from '@/components/common/HouseCard';
 import { GetHouses } from '@/modules/services/api/get/GetHouses';
 import React, { useEffect, useState } from 'react'
 
@@ -18,10 +17,11 @@ const MortgageRentList = () => {
         <div className='flex flex-wrap gap-6 w-full mt-10'>
             {data.houses?.map((property: any) => (
                 <div dir="rtl" key={property.id} className="shrink-0 w-[calc(100%-20px)] md:w-[calc(33.333%-16px)]">
-                    <FastReserveCard className="w-full" property={property}/>
+                    <HouseCard className="w-full" property={property} transactionType="rental"/>
                 </div>
             ))}        
         </div>
     )
+}
 
 export default MortgageRentList;
