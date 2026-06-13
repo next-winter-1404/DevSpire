cff7fae7d6f0bfcd1eef698d900eaf1b63001eb8
Parsa <mparsabahredar@gmail.com>
2026-06-13 15:54:09 +0330

    work on CustomerDashboard

--- src/modules/home/components/SayAboutUsSlider.tsx
+++ src/modules/home/components/SayAboutUsSlider.tsx
@@ -1,17 +1,64 @@
-import { apiFetch } from "@/core/Server-fetch/fetchApi";
-import SayAboutUsSliderClient from "./SayAboutUsSliderClient";
+"use client";
+import React, { useState } from "react";
+import SayAboutUsCard from "./SayAboutUsCard";
+import BigArrowRight from "../../../../public/icons/BigArrowRight";
+import BigArrowLeft from "../../../../public/icons/BigArrowLeft";
+import { useLocale } from "next-intl";
+import SliderWrapper from "@/components/common/SliderWrapper";
 
-const SayAboutUsSlider = async () => {
-  const data = await apiFetch("/comments", {
-    params: {
-      limit: 5,
-    },
-    next: {
-      revalidate: 60,
-    },
-  });
+interface Property {
+  id: number;
+  [key: string]: any;
+}
 
-  return <SayAboutUsSliderClient initialData={data.comments} />;
+interface Props {
+  data: Property[];
+}
+
+
+const SayAboutUsSlider = ({ data } : Props) => {
+
+  const locale = useLocale();
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const itemsPerView = 3;
+  const totalSlides = Math.ceil(data.length / itemsPerView);
+
+  const nextSlide = () => {
+    if (currentIndex < totalSlides - 1) {
+      setCurrentIndex((prev) => prev + 1);
+    }
+  };
+
+  const prevSlide = () => {
+    if (currentIndex > 0) {
+      setCurrentIndex((prev) => prev - 1);
+    }
+  };
+
+  return (
+    <div className="flex flex-col items-center gap-10 w-full">
+      <SliderWrapper>
+        {data?.map((data: any) => (
+          <div
+            className='shrink-0 w-[calc(100%-20px)] md:w-[calc(33.333%-16px)]'
+            dir='rtl'
+            key={data.id}
+          >
+            <SayAboutUsCard data={data}/>
+          </div>
+        ))}
+      </SliderWrapper>
+      <div className="flex gap-8">
+        <div onClick={nextSlide}>
+          <BigArrowRight className={locale === "en" ? "scale-x-[-1]" : ""} />
+        </div>
+        <div onClick={prevSlide}>
+          <BigArrowLeft className={locale === "en" ? "scale-x-[-1]" : ""} />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SayAboutUsSlider;
